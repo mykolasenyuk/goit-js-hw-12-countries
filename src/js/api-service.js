402da9c4c6169e32e5b99b40f46d4e7ab5a7c9fc
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-05-14 22:16:11 +0100

    added PNotify

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,14 +1,26 @@
 
+import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js'
+
+console.log(PNotify.notice())
 export default class ApiServise{
     constructor() {
         this.searchQuery = '';
      }
     
     fetchCountries() {
-        console.log(this)
+        // console.log(this)
         const BASE_URL = `https://restcountries.eu/rest/v2/name/${this.searchQuery}`;
           return fetch(BASE_URL)
-        .then(response => response.json())
+              .then(response => {
+                  if (response.ok) {
+                      return response.json();
+                  }
+                  else {
+                      PNotify.error({
+          text: '404 Country is not found!',
+        })}
+              }       
+              )
              
     }
     get query() {
--- src/js/contries.js
+++ src/js/contries.js
@@ -1,7 +1,8 @@
 import countryCardTemplate from '../templates/country-card.hbs';
-import countiesCardTemplate from '../templates/countries.hbs'
-import ApiServise from '../js/api-service'
-
+import countiesCardTemplate from '../templates/countries.hbs';
+import ApiServise from '../js/api-service';
+import { debounce } from "lodash";
+import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js'
 
 
 
@@ -17,18 +18,21 @@ console.log(refs.countriesList);
 const apiServise = new ApiServise()
 
 
-refs.input.addEventListener('input', onSearch);
+refs.input.addEventListener('input', debounce(onSearch,500));
 
 function onSearch(e) {
-    e.preventDefault();
+    // e.preventDefault();
     clearCardList();
 
     apiServise.query = e.target.value;
    
     apiServise.fetchCountries().then(data => {
-        console.log(data)
+        // console.log(data)
         if (data.length > 10) {
-           return alert('too many') 
+            PNotify.error({
+          text: 'Too many matches found. Please enter a more spesific query!',
+        })
+      
         }else if (data.length > 1) {
            countriesMarup(data) 
         }
@@ -47,10 +51,7 @@ function countryMarup(items) {
     refs.countriesList.insertAdjacentHTML
     ('beforeend',countryCardTemplate(items))
 }
-//
-// function createCoutriesListMarup(items) {
-//   return countiesCardTemplate(items);
-// }
+
 
 
 
